4c2535fb87e864e2f0f3cb0a11d87b22db56c773
jorge <jorge.fuenzalida@uc.cl>
2023-05-24 22:42:00 -0400

    muestra datos

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,6 @@
 import ContactForm from '../components/contact_form';
+import {useState, useEffect} from "react";
+import {useUser} from "../context/userContext";
 
 const ContactPage = () => {
   const handleContactSubmit = (phoneNumber: string) => {
@@ -6,10 +8,31 @@ const ContactPage = () => {
     console.log('phone number?' + phoneNumber)
   };
 
+  const { user } = useUser();
+
+  const [datos, setDatos] = useState('no hay datos');
+
+  const clickHandler = () => {
+    if (user === null) {
+      setDatos('user null');
+    }
+    else
+    {
+      setDatos(user.token);
+    }
+  }
+
+  useEffect(() => {
+
+  }, [user])
+
+
   return (
       <div>
         <h1>Contact Page</h1>
         <ContactForm onSubmit={handleContactSubmit} />
+        <button onClick={clickHandler}>Ver si tenemos acceso datos</button>
+        <>{datos}</>
       </div>
   );
 };
